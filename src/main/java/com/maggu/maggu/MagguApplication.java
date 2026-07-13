a9d93241c44a721957404fa01a3711f950acdf67
sijin1017 <trsj1017@naver.com>
2026-07-14 00:21:22 +0900

    [Feat] 프로젝트 초기 세팅

---
+++ src/main/java/com/maggu/maggu/MagguApplication.java
@@ -0,0 +1,13 @@
+package com.maggu.maggu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagguApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maggu/maggu/MagguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maggu.maggu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
