e9da500111c1cd4fd3a9e1bcda347d1d17c0b994
joongwonAn <anjoongwon517@gmail.com>
2026-07-23 15:50:27 +0900

    feat: UserRepository에 사용자 제공자 조회 기능 추가
    - Provider와 ProviderUserId를 기준으로 AppUser 조회를 위한 findByProviderAndProviderUserId 메서드 추가
    - 외부 제공자 기반 사용자 인증 및 조회 기능 강화

--- src/main/java/com/maggu/maggu/user/repository/UserRepository.java
+++ src/main/java/com/maggu/maggu/user/repository/UserRepository.java
@@ -1,11 +1,16 @@
 package com.maggu.maggu.user.repository;
 
+import com.maggu.maggu.global.entity.enums.Provider;
 import com.maggu.maggu.user.entity.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<AppUser, Long> {
 
     boolean existsByNickname(String nickname);
+
+    Optional<AppUser> findByProviderAndProviderUserId(Provider provider, String providerUserId);
 }
