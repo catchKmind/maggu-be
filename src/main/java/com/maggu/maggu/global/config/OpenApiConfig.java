96661ac96d7cf136e68056cd74456e93e5640256
joongwonAn <anjoongwon517@gmail.com>
2026-07-26 17:10:23 +0900

    feat(config): OpenAPI에 JWT 인증 스키마 추가
    - Swagger 문서에 JWT 인증을 위한 BearerAuth 스키마와 SecurityRequirement 추가
    - API 보안을 강화하고 인증 필요 여부를 문서화하여 사용성 향상

--- src/main/java/com/maggu/maggu/global/config/OpenApiConfig.java
+++ src/main/java/com/maggu/maggu/global/config/OpenApiConfig.java
@@ -1,12 +1,17 @@
 package com.maggu.maggu.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
+    private static final String BEARER_AUTH_SCHEME = "bearerAuth";
 
     @Bean
     public OpenAPI openAPI() {
@@ -15,6 +20,12 @@ public class OpenApiConfig {
                         .title("maggu API")
                         .description("여행 사진 커뮤니티 서비스 API 문서. 모든 응답은 공통 포맷(ApiResponse)으로 감싸져 내려가며, "
                                 + "실패 응답의 code는 도메인 접두사(USER-xxx, POST-xxx 등) 또는 COMMON-xxx로 구분된다.")
-                        .version("v0.0.1"));
+                        .version("v0.0.1"))
+                .components(new Components()
+                        .addSecuritySchemes(BEARER_AUTH_SCHEME, new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH_SCHEME));
     }
 }
