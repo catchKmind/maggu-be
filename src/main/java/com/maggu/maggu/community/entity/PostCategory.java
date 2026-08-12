d21b93adfbcada5579c2ef91fbb6f76d160af75a
sijin1017 <trsj1017@naver.com>
2026-08-12 19:30:10 +0900

    [Fix] 큐레이션 상단 탭 카테고리 변경

--- src/main/java/com/maggu/maggu/community/entity/PostCategory.java
+++ src/main/java/com/maggu/maggu/community/entity/PostCategory.java
@@ -1,8 +1,8 @@
 package com.maggu.maggu.community.entity;
 
 public enum PostCategory {
-    NOTICE,     // 운영 - 추천 장소/서비스 공지, 관리자만 작성 가능(서비스 계층에서 권한 체크)
-    QNA,        // 질문/정보
-    RECOMMEND   // 추천 - 개인 여행담/추천 스레드
+    CURATION,   // 큐레이션 - 키워드/주제별 묶음
+    INFO,       // 정보 - 일시, 휴무, 개최 정보 등 (기존 QNA 대체/통합)
+    RECOMMEND   // 추천 - 여행 코스, 경로, 추천 장소
 }
 
