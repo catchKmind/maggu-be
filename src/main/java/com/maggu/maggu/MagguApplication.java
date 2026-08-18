0fc1176d5a63ec64d642894812316619690c3a1f
joongwonAn <anjoongwon517@gmail.com>
2026-08-18 13:44:18 +0900

    feat(core): @ConfigurationPropertiesScan 애너테이션 추가 및 코드 정리
    - 프로퍼티 클래스 스캔을 활성화하기 위해 `@ConfigurationPropertiesScan` 애너테이션을 추가
    - 코드 가독성을 위해 메인 메서드 내 들여쓰기 수정
    - Spring Boot 애플리케이션의 설정 관리 및 확장성을 개선

--- src/main/java/com/maggu/maggu/MagguApplication.java
+++ src/main/java/com/maggu/maggu/MagguApplication.java
@@ -2,14 +2,16 @@ package com.maggu.maggu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@ConfigurationPropertiesScan
 @EnableJpaAuditing
 @SpringBootApplication
 public class MagguApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MagguApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MagguApplication.class, args);
+    }
 
 }
