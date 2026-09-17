17842278bfdf717ce07451e4f7bdd99d7944ba7e
joongwonAn <anjoongwon517@gmail.com>
2026-09-17 15:35:19 +0900

    feat(repository): PlaceFolderRepository 인터페이스 추가
    - `PlaceFolder` 엔티티의 데이터를 관리하기 위한 JPA 리포지토리 추가
    - 사용자의 폴더를 기본 폴더 여부와 생성일 기준으로 정렬하여 조회하는 메서드 구현

---
+++ src/main/java/com/maggu/maggu/place/repository/PlaceFolderRepository.java
@@ -0,0 +1,12 @@
+package com.maggu.maggu.place.repository;
+
+import com.maggu.maggu.place.entity.PlaceFolder;
+import com.maggu.maggu.user.entity.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PlaceFolderRepository extends JpaRepository<PlaceFolder, Long> {
+
+    List<PlaceFolder> findAllByUserOrderByIsDefaultDescCreatedAtAsc(AppUser user);
+}
